6653c06c0125582b5f2a219a20758152fcb5b153
mira-kine <mirak0419@gmail.com>
2022-02-15 12:56:30 -0800

    edit works, but want to clean up later

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
-// import { useState } from 'react';
+import { useState } from 'react';
 import './List.css';
 
 export default function List({ items, handleUpdate }) {
-  // edit button handleUpdate
+  const [edit, setEdit] = useState(false);
+  console.log('edit', edit);
   return (
     <div>
       <ul className="list-container">
-        {items.map((item) => (
-          <li key={item.id} className="list-item">
-            <input type="checkbox" />
-            <input
-              value={item.text}
-              onChange={(e) => {
-                handleUpdate({ ...item, text: e.target.value });
-              }}
-            />
-            <button onClick={handleUpdate}>Edit</button>
-          </li>
-        ))}
+        {items.map((item) => {
+          if (edit === true) {
+            return (
+              <li key={item.id} className="list-item">
+                <input type="checkbox" />
+                <input
+                  value={item.text}
+                  onChange={(e) => {
+                    handleUpdate({ ...item, text: e.target.value });
+                  }}
+                />
+                <button onClick={() => setEdit(false)}>Save</button>
+              </li>
+            );
+          } else {
+            return (
+              <li key={item.id} className="list-item">
+                <input type="checkbox" />
+                <p>{item.text}</p>
+                <button onClick={() => setEdit(true)}>Edit</button>
+              </li>
+            );
+          }
+        })}
       </ul>
     </div>
   );
